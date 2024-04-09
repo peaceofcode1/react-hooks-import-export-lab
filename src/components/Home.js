757b80a19d3f037e973aa6151d125ed278792f9d
jafar bino <binocoder@gmail.com>
2024-04-09 21:44:14 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,13 @@ import React from "react";
 
 function Home() {
   return (
+    
     <div id="home">
       <h1>
         {username} is a Web Developer from {city}
       </h1>
     </div>
   );
+ 
 }
+export default Home;
